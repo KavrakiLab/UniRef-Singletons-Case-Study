3458a3147dc2e8c38cbbf0014c2799e01a33dbc1
Felix Quintana <44539035+FelixMQuintana@users.noreply.github.com>
2026-01-05 10:30:45 -0600

    Update workflow.py
    remove uneccessary imports

--- src/workflow.py
+++ src/workflow.py
@@ -5,8 +5,6 @@ import re
 import shutil
 from pathlib import Path
 from typing import Optional, Tuple
-import plotly.graph_objects as go
-import plotly.express as px
 import pandas as pd
 import numpy as np
 import subprocess
